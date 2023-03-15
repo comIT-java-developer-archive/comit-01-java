be510490ec7477190c8de9a58bdc7dc9305240c0
Carlos Zegarra <cgzegarra@gmail.com>
2023-03-15 01:39:10 -0400

    sorting

---
+++ src/main/java/com/comit/course/_17_collections/_01_lists/CopyListExample.java
@@ -0,0 +1,40 @@
+package com.comit.course._17_collections._01_lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CopyListExample {
+	
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> list1 = new ArrayList<>();
+		
+		list1.add("John");
+		list1.add("Jane");
+		list1.add("Pete");
+		list1.add("Mike");
+		list1.add("Lily");
+		
+		
+		ArrayList<String> list2 = (ArrayList<String>) list1.clone();
+		
+		list1.set(2, "Hello");
+		
+		System.out.println(list2);
+		/*System.out.println(list1.size());
+		
+		
+		for(String str: list1) {
+			list2.add(str);
+		}
+		
+		list1.clear();
+		System.out.println(list1);
+		System.out.println(list2);
+		System.out.println(list2.size());*/
+		
+	}
+
+}
--- src/main/java/com/comit/course/_17_collections/_03_maps/PersonMapExample.java
+++ src/main/java/com/comit/course/_17_collections/_03_maps/PersonMapExample.java
@@ -1,9 +1,9 @@
 package com.comit.course._17_collections._03_maps;
 
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import com.comit.course._17_collections.Person;
 
@@ -29,8 +29,12 @@ public class PersonMapExample {
 		
 		/*
 		 * Map are useful when we need to search by a key.
+		 * 
+		 * HashMap: 
+		 * LinkedHashMap: Keeps the insertion order of the entry
+		 * TreeMap: Sorts the map by Key.
 		 */
-		Map<Integer,Person> map = new Hashtable<>();
+		Map<Integer,Person> map = new TreeMap<>();
 		
 		map.put(2, new Person(2,"John",30));
 		map.put(4, new Person(4,"Omar",30));
@@ -40,19 +44,20 @@ public class PersonMapExample {
 		map.put(10, new Person(10,"Lily",25));
 		
 		System.out.println(map.get(5));
+		
+		map.put(10, new Person(10,"Hello",30));
+		
+		// Loop over a map - As of Java 8
+		//map.forEach((k,v)->System.out.println(k + " , " + v));
+		
+		
+		for(Integer key: map.keySet()) {
+			System.out.println(key + " , " + map.get(key));
+		}
+		
+		System.out.println(map);
 	}
 	
-	void hello() {
-		Map<Integer,Person> map = new Hashtable<>();
-		
-		map.put(2, new Person(2,"John",30));
-		map.put(4, new Person(4,"Omar",30));
-		map.put(7, new Person(7,"Jane",20));
-		map.put(3, new Person(3,"Mike",36));
-		map.put(5, new Person(5,"Pete",28));
-		map.put(10, new Person(10,"Lily",25));
-		
-		System.out.println(map.get(5));	
-	}
+
 
 }
